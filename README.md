39d3df9c5849e0eb4ced113f0461f2c0cbbce68d
RIX <rix@rixtop.rixnet>
2024-06-25 23:26:39 -0400

    removed some things I don't need and I changed some scripts to install the deb version

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+#rix-deb
+This isn't meant to be used by anyone else. I'm taking the pieces of this project that I like and use and migrating into my dotfiles and stuff.
+
 # Omakub
 
 Turn a fresh Ubuntu installation into a fully-configured, beautiful, and modern web development system by running a single command. That's the one-line pitch for Omakub. No need to write bespoke configs for every essential tool just to get started or to be up on all the latest command-line tools. Omakub is an opinionated take on what Linux can be at its best.
--- install/ulauncher.sh
+++ install/ulauncher.sh
@@ -1,11 +1,7 @@
-sudo add-apt-repository universe -y
-sudo add-apt-repository ppa:agornostal/ulauncher -y
-sudo apt update -y
-sudo apt install -y ulauncher
-
-# Start ulauncher to have it populate config before we overwrite
-mkdir -p ~/.config/autostart/
-cp ~/.local/share/omakub/configs/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
-gtk-launch ulauncher.desktop >/dev/null 2>&1
-sleep 2 # ensure enough time for ulauncher to set defaults
-cp ~/.local/share/omakub/configs/ulauncher.json ~/.config/ulauncher/settings.json
+sudo apt update && sudo apt install -y gnupg
+gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
+gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] \
+          http://ppa.launchpad.net/agornostal/ulauncher/ubuntu jammy main" \
+          | sudo tee /etc/apt/sources.list.d/ulauncher-jammy.list
+sudo apt update && sudo apt install ulauncher
