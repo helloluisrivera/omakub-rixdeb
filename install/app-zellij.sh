99394d262849f1fc7d112a8a68362eca439d539b
RIX <rix@rixtop.rixnet>
2024-06-26 00:03:30 -0400

    remove zellij dotfiles

--- install/app-zellij.sh
+++ install/app-zellij.sh
@@ -4,7 +4,3 @@ tar -xf zellij.tar.gz zellij
 sudo install zellij /usr/local/bin
 rm zellij.tar.gz zellij
 cd -
-
-mkdir -p ~/.config/zellij/
-[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp ~/.local/share/omakub/configs/zellij.kdl ~/.config/zellij/config.kdl
-[ ! -L "$HOME/.config/zellij/themes" ] && ln -s ~/.local/share/omakub/themes/zellij ~/.config/zellij/themes
