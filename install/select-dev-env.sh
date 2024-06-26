6355fcd5fc5c9d00481340ca593616c23f329896
RIX <rix@rixtop.rixnet>
2024-06-26 00:11:01 -0400

    removing code and docker

--- install/select-dev-env.sh
+++ install/select-dev-env.sh
@@ -12,7 +12,6 @@ for language in $languages; do
 		;;
 	Go)
 		mise use --global go@latest
-		code --install-extension golang.go
 		;;
 	Java)
 		mise use --global java@latest
@@ -31,23 +30,6 @@ for language in $languages; do
 		php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 		php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
 		rm composer-setup.php
-		;;
-	esac
-done
-
-# Install default databases
-dbs=$(gum choose "MySQL" "Redis" "PostgreSQL" --no-limit --selected "MySQL","Redis" --height 5 --header "Select databases (runs in Docker)")
-
-for db in $dbs; do
-	case $db in
-	MySQL)
-		sudo docker run -d --restart unless-stopped -p "127.0.0.1:3306:3306" --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8.4
-		;;
-	Redis)
-		sudo docker run -d --restart unless-stopped -p "127.0.0.1:6379:6379" --name=redis redis:7
-		;;
-	PostgreSQL)
-		sudo docker run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16
 		;;
 	esac
 done
